3ce2b42ba7d5bff8f8befdcbef8ba0884da5f2c9
dreamtrue <dreamtrue@gmail.com>
2014-02-15 10:46:20 -0800

    补充一些没有列入版本的文件。本版与上一个版本之间其实没有改动。

---
+++ HiStar/Color.h
@@ -0,0 +1,20 @@
+// Color.h
+// Colorref's to use with your Programs
+
+#define RED        RGB(255,  0,  0)
+#define GREEN      RGB(  0,255,  0)
+#define BLUE       RGB(  0,  0,255)
+#define LITRED   RGB(204,  51,  51)
+#define YELLOW		RGB(255,  255,  0)
+#define LITGREEN RGB(  0,128,  0)
+#define LITBLUE  RGB(  0,  0,128)
+#define BLACK      RGB(  0,  0,  0)
+#define WHITE      RGB(255,255,255)
+#define GRAY       RGB(192,192,192)
+#define LITGRAY     RGB(240,240,240)
+#define RGB_SEL		RGB(128,255,255)
+#define ACC_BG		RGB(109,109,109)
+#define DLG_GRAY	RGB(240,240,240)
+#define TXT_CANCEL	RGB(128,128,0)
+//#define TXT_UNTRADE RGB(128,128,0)
+#define PART_TRADE RGB(0,128,128)
