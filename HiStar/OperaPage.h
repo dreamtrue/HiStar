3ac672b8d0d7d8bdffb8ed00df1b050569540f53
dreamtrue <dreamtrue@gmail.com>
2014-02-16 17:09:51 -0800

    头文件路径做了小修改。

--- HiStar/OperaPage.h
+++ HiStar/OperaPage.h
@@ -7,7 +7,7 @@
 #include "HScrollListBox.h" 
 #include "afxcmn.h"
 #include "EWrapper.h" 
-#include "e:\histar\trunk\cppclient\shared\hscrolllistbox.h"
+#include "hscrolllistbox.h"
 
 // COperaPage ¶Ô»°¿ò
 class COperaPage : public CDialogEx
