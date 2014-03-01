25b151c7987db0e4712f1f68e04ae5428bf63fe1
dreamtrue <dreamtrue@gmail.com>
2014-03-01 06:13:18 -0800

    增加一个头文件。

---
+++ Hedge/HedgePostProcessing.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// CHedgePostProcessing
+
+class CHedgePostProcessing : public CWinThread
+{
+	DECLARE_DYNCREATE(CHedgePostProcessing)
+
+protected:
+	CHedgePostProcessing();           // 动态创建所使用的受保护的构造函数
+	virtual ~CHedgePostProcessing();
+
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+public:
+	void PostProcessing(UINT wParam,LONG lParam);//后处理
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+
